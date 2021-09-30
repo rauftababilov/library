09e9bc37ffe5d889a6796343c052853826bd77c7
KyrylovDV <d.kyrylov.andersen@gmail.com>
2021-09-30 15:11:42 +0300

    Revert "Add RecordKeeping, Client entities, move classes to their packages"
    This reverts commit de1218b6dcb40f00717909c0f95b8421a6678be2.

---
+++ src/main/java/com/andersen/library/jpa/repository/PublishingHouseRepository.java
@@ -0,0 +1,7 @@
+package com.andersen.library.jpa.repository;
+
+import com.andersen.library.jpa.domain.PublishingHouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublishingHouseRepository extends JpaRepository<PublishingHouse, Long> {
+}
--- src/main/java/com/andersen/library/jpa/repository/UserRepository.java
+++ src/main/java/com/andersen/library/jpa/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.andersen.library.services.user.impl;
+package com.andersen.library.jpa.repository;
 
-import com.andersen.library.services.user.impl.User;
+import com.andersen.library.jpa.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
