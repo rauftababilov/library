931af58d23576ce0374082916de68addc742162f
KyrylovDV <d.kyrylov@infostroy.com.ua>
2021-10-04 09:12:00 +0300

    Fix book duplicates

--- src/main/java/com/andersen/library/services/book/impl/BookRepository.java
+++ src/main/java/com/andersen/library/services/book/impl/BookRepository.java
@@ -4,12 +4,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
+import javax.persistence.QueryHint;
 import java.util.Set;
 
 interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query("select b from Book b inner join b.authorIds a" +
+    @QueryHints(value = {
+            @QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false")
+    })
+    @Query("select distinct b from Book b inner join b.authorIds a" +
             " where b.deleted = false " +
             " and (:publishingHouseId is null or b.publishingHouseId in (:publishingHouseId))" +
             " and (:authorId is null or a in (:authorId))" +
